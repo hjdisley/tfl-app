5ca9933f12cfc1f4d2c56016de7140c73120c7fc
Hayden Disley <haydendisley@Haydens-MacBook-Pro.local>
2021-06-20 12:00:43 +0100

    added api request

--- src/components/ServiceTable.js
+++ src/components/ServiceTable.js
@@ -1,32 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import moment from "moment"
+import axios from "axios"
 
 const ServiceTable = () => {
-  const [status, getStatus] = useState([]);
+  const [status, setStatus] = useState([])
+
+  const fetchData = async () => {
+    await axios("https://api.tfl.gov.uk/line/mode/tube/status")
+      .then((res) => setStatus(res.data))
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div>
       <div className="lastUpdated">
-        <p>Last Updated: 27/09/1995</p>
+        <p>Last Updated: {moment().format("MMMM Do YYYY, h:mm:ss a")}</p>
       </div>
       <table>
-        <tr>
+        <thead>
           <th>Line</th>
           <th>Status</th>
           <th></th>
-        </tr>
-        <tr>
-          <td>Bakerloo</td>
-          <td>Good Service</td>
-          <td>
-            <Link to="/travel/bakerloo">
-              <button>View More</button>
-            </Link>
-          </td>
-        </tr>
+        </thead>
+        <tbody>
+          {status.map((line) => {
+            return (
+              <tr>
+                <td>{line.name}</td>
+                <td>{line.lineStatuses[0].statusSeverityDescription}</td>
+                <td>
+                  <Link to={`/travel/${line.name}`}>
+                    <button>View More</button>
+                  </Link>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default ServiceTable;
+export default ServiceTable
+
+{
+  /* <td>Bakerloo</td>
+<td>Good Service</td>
+<td>
+  <Link to="/travel/bakerloo">
+    <button>View More</button>
+  </Link>
+</td> */
+}
